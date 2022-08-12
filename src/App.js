4d58e620acff41376d85968c8ab06808a997a1ae
GUchechi <godswilluchendu7@gmail.com>
2022-08-12 23:38:24 +0100

    imported header, note and footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Note from './Components/Note';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Note />
+      <Footer /> 
     </div>
   );
 }
