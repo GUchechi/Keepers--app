9be4dbbfc8f2293ed25f9ffa63d77ffddf50d603
GUchechi <godswilluchendu7@gmail.com>
2022-08-23 01:27:21 +0100

    created a constant that keeps track of the title & content

--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -5,11 +5,24 @@ function CreateArea() {
         title: "",
         content: "",
     })
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value,
+            };
+        })
+    }
+
+
   return (
     <div>
       <form>
-        <input name="title" value={note.title} placeholder="Title" />
-        <textarea name="content" value={note.content} placeholder="Take a note..." rows="3" />
+        <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />
+        <textarea onChange={handleChange}  name="content" value={note.content} placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
